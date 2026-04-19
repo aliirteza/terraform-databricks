66a319deb5d8e502119065eb71a10deaf58763a6
aliirteza <ali.irteza93@gmail.com>
2026-04-19 20:49:02 +0100

    redo main.tf minimal

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,54 @@
-resource "random_string" "naming" {
-    length = 6
-    special = false
-    upper = false
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
-data "external" "me" {
-  program = ["az", "account", "show", "--query", "user"]
+provider "azurerm" {
+  features {}
+}
+
+variable "region" {
+  type    = string
+  default = "westeurope"
+}
+
+resource "random_string" "naming" {
+  length  = 6
+  upper   = false
+  special = false
 }
 
 locals {
   prefix = "databricksdemo${random_string.naming.result}"
-
   tags = {
     Environment = "Demo"
-    Owner       = lookup(data.external.me.result, "name")
+    Owner       = "ali.ir"
   }
 }
 
+resource "azurerm_resource_group" "this" {
+  name     = "${local.prefix}-rg"
+  location = var.region
+  tags     = local.tags
+}
+
+resource "azurerm_databricks_workspace" "this" {
+  name                        = "${local.prefix}-workspace"
+  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this.location
+  sku                         = "premium"
+  managed_resource_group_name = "${local.prefix}-workspace-rg"
+  tags                        = local.tags
+}
+
+output "databricks_host" {
+  value = "https://${azurerm_databricks_workspace.this.workspace_url}/"
+}
